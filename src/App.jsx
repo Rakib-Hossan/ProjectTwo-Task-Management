38241aeee9a2efc8a44391d25754673ce79ae37f
Rakib-Hossan <rakib.redoy01@gmail.com>
2024-10-17 11:38:27 +0600

    complete UI design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import TaskBoard from "./Task/TaskBoard";
 
 export default function App() {
   return (
-    <>
+    <fragment className="flex flex-col justify-center items-center">
       <Header />
       <HeroSection />
+      <TaskBoard />
       <Footer />
-    </>
+    </fragment>
   );
 }
